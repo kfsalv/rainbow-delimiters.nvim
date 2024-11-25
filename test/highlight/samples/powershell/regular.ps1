d574d8c19f51b5156835e757327da74e608fc072
Kenneth Frank Salvesen <kenneth.f.salvesen@ntnu.no>
2024-11-25 17:14:32 +0100

    tests

--- test/highlight/samples/powershell/regular.ps1
+++ test/highlight/samples/powershell/regular.ps1
@@ -12,13 +12,13 @@ using namespace System.Collections.Generic
 # function_parameter_declaration
 function test
 {
-	# param_block
-	param([string]$parameter)
+    # param_block
+    param([string]$parameter)
 }
 
 function simple($testValue)
 {
-	Write-Host $testValue
+    Write-Host $testValue
 }
 
 
@@ -26,35 +26,35 @@ function simple($testValue)
 foreach ($item in $collection)
 # statement_block
 {
-	# for_statement
-	for ($i = 0; $i -lt $array.Count; $i++)
-	# statement_block
-	{
-		# while_statement
-		while ($false)
-		# statement_block
-		{
-			# do_statement
-			do
-			# statement_block
-			{
-				do
-				# statement_block
-				{
-					# if_statement
-					if (1)
-					{
+    # for_statement
+    for ($i = 0; $i -lt $array.Count; $i++)
+    # statement_block
+    {
+        # while_statement
+        while ($false)
+        # statement_block
+        {
+            # do_statement
+            do
+            # statement_block
+            {
+                do
+                # statement_block
+                {
+                    # if_statement
+                    if (1)
+                    {
     
-					}
-					# elseif_clause
-					elseif (2)
-					{
+                    }
+                    # elseif_clause
+                    elseif (2)
+                    {
     
-					}
-				} while ($false)
-			} until ($false) 
-		}
-	}
+                    }
+                } while ($false)
+            } until ($false) 
+        }
+    }
 }
 
 
@@ -68,69 +68,72 @@ foreach ($item in $collection)
 switch ($x)
 # switch_body
 {
-	1 {  }
-	Default {}
+    1 {  }
+    Default {}
 }
 
 # class_statement
 class ClassName
 {
-	[int] $value
-	# class_method_definition ()
-	# class_method_definition []
-	ClassName([int]$val) 
-	{
-		$this.value = $val
+    [int] $value
+    # class_method_definition ()
+    # class_method_definition []
+    ClassName([int]$val) 
+    {
+        $this.value = $val
 
-	}
-	static [void] MethodName([string]$OptionalParameters)
-	{
+    }
+    static [void] MethodName([string]$OptionalParameters)
+    {
 
-	}
-	Dummy()
-	{
-		$this.value++
-	}
+    }
+    Dummy()
+    {
+        $this.value++
+    }
 
-	[int] $PropertyName
-	hidden [string] $PropertyName
+    [int] $PropertyName
+    hidden [string] $PropertyName
 
 }
 
 # enum_statement
 enum EnumName
 {
-	ValueString = 0
-	Value2String = 1
+    ValueString = 0
+    Value2String = 1
 }
 
 # attribute []
 # attribute ()
 function Get-Stuff
 {
-	[CmdletBinding(DefaultParameterSetName = 'stuff', PositionalBinding = $false)]
-	[Alias('stuff')]
-	[OutputType([String])]
-	Param (
-		[Parameter(Mandatory, ValueFromPipeline, ParameterSetName = 'stuff')]
-		[ValidateNotNullOrEmpty()]
-		[ValidateCount(0, 5)]
-		[ValidateSet('sun', 'moon', 'earth')]
-		[Alias('p1')]
-		[string[]]
-		$Param1,
+    [CmdletBinding(DefaultParameterSetName = 'stuff', PositionalBinding = $false)]
+    [Alias('stuff')]
+    [OutputType([String])]
+    Param (
+        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName = 'stuff')]
+        [ValidateNotNullOrEmpty()]
+        [ValidateCount(0, 5)]
+        [ValidateSet('sun', 'moon', 'earth')]
+        [Alias('p1')]
+        [string[]]
+        $Param1,
         
-		[Parameter(ParameterSetName = 'stuff')]
-		[AllowNull()]
-		[ValidateScript({ $true })]
-		[ValidateRange(0, 5)]
-		[int]
-		$Param2
-	)
+        [Parameter(ParameterSetName = 'stuff')]
+        [AllowNull()]
+        [ValidateScript({ $true })]
+        [ValidateRange(0, 5)]
+        [int]
+        $Param2
+    )
 }
 
 # element_access
+$array[0]
+$array[0][0]
 $array[0][0][0]
+$array[0][0][0][0]
 $array[0, 0, 0]
 
 # type_literal
@@ -148,10 +151,10 @@ $hashListDict = [HashSet[List[Dictionary[string, [List[int]]]]]]::new()
 [System.Collections.Generic.List[int]]$list = @( 1, 2, 3, 4, 5 )
 
 $result = [System.Linq.Enumerable]::Select[int, float](
-	$list,
-	[Func[int, float]]{
-		param($item)
-		[math]::Pow($item, 3)
-	}
+    $list,
+    [Func[int, float]]{
+        param($item)
+        [math]::Pow($item, 3)
+    }
 )
 $result
